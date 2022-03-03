3736b748c77eb9aff5f267bbf757d975f07578fe
Jonathan Grimaldo <jonathan.grimaldo@kiubix.com>
2022-03-02 22:34:10 -0600

    Seccion lista de pasteles

--- src/app/pages/cakelist/cakelist.component.ts
+++ src/app/pages/cakelist/cakelist.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cakelist',
   templateUrl: './cakelist.component.html',
   styleUrls: ['./cakelist.component.css']
 })
-export class CakelistComponent implements OnInit {
+export class CakelistComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
+
