f4bce493028f38776fed5ea0e01c4669afd6b13c
Jonathan Grimaldo <jonathan.grimaldo@kiubix.com>
2022-03-04 00:00:48 -0600

    reviws section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
